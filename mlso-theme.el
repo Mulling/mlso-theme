4ec97d6457b97db1ffadcc6d18553c6772a97257
mulling <lucas_mulling@hotmail.com>
2020-03-20 23:35:34 -0300

    remove the ascii art

--- mlso-theme.el
+++ mlso-theme.el
@@ -7,14 +7,7 @@
 
 ;;; Commentary:
 
-;;            _                 _   _                               _
-;;  _ __ ___ | |___  ___       | |_| |__   ___ _ __ ___   ___   ___| |
-;; | '_ ` _ \| / __|/ _ \ _____| __| '_ \ / _ \ '_ ` _ \ / _ \ / _ \ |
-;; | | | | | | \__ \ (_) |_____| |_| | | |  __/ | | | | |  __/|  __/ |
-;; |_| |_| |_|_|___/\___/       \__|_| |_|\___|_| |_| |_|\___(_)___|_|
-
 ;; A dark, medium contrast theme with warm colors.
-;;
 ;; This work is licensed under the WTFPL.  See <http://www.wtfpl.net/about/>.
 ;; This is also a work in progress, so things may change.
 
@@ -40,7 +33,7 @@
 
    (custom-theme-set-faces
     'mlso
-    
+
     `(default     ((t :background ,black :foreground ,white)))
     `(cursor      ((t :background ,orange :foreground ,black)))
     `(fringe      ((t :inherit default :background ,black :foreground ,gray)))
